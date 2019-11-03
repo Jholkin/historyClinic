78ca33cc8ec322329c4d03b10d3ccbdb1c11445f
jholkin <palaciosmarianojhil@gmail.com>
2019-11-03 12:17:57 -0500

    relationship between Role and Permission, User

--- app/Role.php
+++ app/Role.php
@@ -7,4 +7,26 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     //
+    protected $fillable = [
+        'name', 'slug', 'description'
+    ];
+
+    //RELATIONSHIP
+    public function permission()
+    {
+        return $this->hasMany('App\Permission');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany('App\User');
+    }
+
+    //STORAGE
+
+    //VALIDATION
+
+    //BACKUP OF INFORMATION
+
+    //OTHER OPERATIONS
 }
